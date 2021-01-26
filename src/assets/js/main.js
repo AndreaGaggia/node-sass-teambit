8b8078e5ef08bbb45c8ccbfff3ca57f07825624c
AndreaG <gaggiandrea@gmail.com>
2021-01-26 17:29:02 +0100

    section "before/after"

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,35 @@
 const app = new Vue({
     el: "#app",
-    data: {},
+    data: {
+        problemCards: [
+            {
+                img: "dist/img/hardcore.png",
+                caption: "They are hardcore, formal and disconnected",
+            },
+            {
+                img: "dist/img/nofeedback.png",
+                caption: "They don't encourage regular feedback",
+            },
+            {
+                img: "dist/img/notsafe.png",
+                caption: "They don't make it safe to speak up",
+            },
+        ],
+        solutionCards: [
+            {
+                img: "dist/img/lightweight.png",
+                caption: "It's lightweight fun and interconnected",
+            },
+            {
+                img: "dist/img/feedbacksuggestions.png",
+                caption:
+                    "It encourages regular feedback with contextual requests",
+            },
+            {
+                img: "dist/img/safe.png",
+                caption: "It creates a safe environment for dialog",
+            },
+        ],
+    },
     methods: {},
 });
